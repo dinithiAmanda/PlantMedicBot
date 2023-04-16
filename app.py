9d45235e1e1800bcb28327eee78dcd556c4e8b40
dinithiAmanda <dinithiama00@gmail.com>
2023-04-16 16:04:35 +0530

    import frontends' libraries

---
+++ app.py
@@ -0,0 +1,10 @@
+from tkinter import *
+import time
+import tkinter.messagebox
+from chatBot import Agro
+import threading
+import random
+
+# username = ["You"]
+# ans = ["AgroBot"]
+window_size = "428x520"
