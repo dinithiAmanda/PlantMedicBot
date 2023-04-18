22a8704ee99f5fed9762884806197046aaca5b18
dinithiAmanda <dinithiama00@gmail.com>
2023-04-18 14:00:53 +0530

    lemmartization the dataset

--- chatBot.py
+++ chatBot.py
@@ -21,6 +21,8 @@ warnings.filterwarnings("ignore")
 # open dataset
 ds = open("dataset.txt", 'r', errors= "ignore")
 
+# normalization
+
 raw = ds.read() # separated sections file
 lraw = raw.lower() # convert to lower case
 
@@ -29,6 +31,16 @@ lraw = raw.lower() # convert to lower case
 
 tokens = nltk.sent_tokenize(lraw) # convert to list of sentences
 
-print(tokens)
+# print(tokens)
 
+# lemmartization
 
+lemmer = nltk.stem.WordNetLemmatizer()
+
+def LemTokens(tokens):
+    return[lemmer.lemmatize(token)for token in tokens]
+
+remove_punct_dict = dict((ord(punct), None)for punct in string.punctuation)
+
+def LemNormalize(text):
+    return LemTokens(nltk.word_tokenize(text.lower().translate(remove_punct_dict)))
